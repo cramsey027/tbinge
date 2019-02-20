c70d6f0f4a39f3775cce62f9884583f2f625136e
Kyle <chiefmccain85@gmail.com>
2019-02-20 09:25:39 -0700

    Initial commit

---
+++ tmdb/tmdb-api-interface.js
@@ -0,0 +1,63 @@
+/* 
+*  This module serves as the primary handler for all TMDb API 
+*  calls and has the project API key set as a constant variable
+*  based on the API key set in the "key.json" file.
+*  Author: Kyle McCain
+*  Date: 19 February 2019
+*/
+const fs = require('fs');
+const request = require("request");
+
+// Declare and initialize TMDb API variables
+const api_key = JSON.parse(fs.readFileSync(__dirname + '/key.json')).api_key;
+const baseURL = 'https://api.themoviedb.org/3/';
+
+// Declare and initialize new error to throw if required parameter is not input
+const isRequired = () => { throw new Error('Parameter is required!')}
+
+// Function that calls TMDb API with the input options
+let tmbdCall = (options, callback) => {
+    // Adds the API key to 'options' prior to calling API
+    options.qs.api_key = api_key;
+
+    // Send request and return the 'body' of response or error message if unsuccessful
+    request(options, (error, response, body) => {
+        // Return error message since no return JSON exists to parse error message
+        if (error) {
+            callback("Unable to connect to TMDb!")
+        // Return error message for status code 401 and 404 from returned JSON
+        } else if (response.statusCode === 401 || response.statusCode === 404) {
+            callback(JSON.parse(body).status_message);
+        // Return object of 'body' if no error in transaction
+        } else {
+            callback(undefined, JSON.parse(body));
+        };
+    });
+};
+
+// Function that returns the correct string to concatenate with the base 
+// URL depending on whether the user is searching for a TV show or a movie
+let getSearchURL = (isTV = isRequired()) => {
+    if (isTV) {
+        return `${baseURL}search/tv`;
+    } else {
+        return `${baseURL}search/movie`;
+    };
+};
+
+// Function that returns the correct string to concatenate with the base URL depending 
+// on whether the user is doing a detailed search for a TV show or a movie
+let getDetailedSearchURL = (isTV = isRequired()) => {
+    if (isTV) {
+        return `${baseURL}tv/`;
+    } else {
+        return `${baseURL}movie/`;
+    };
+};
+
+// Export functions for use outside of module
+module.exports = {
+    tmbdCall,
+    getSearchURL,
+    getDetailedSearchURL
+};
